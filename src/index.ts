5ff1ea8a3fe24130fa43b780ed6e1f58f240190d
Ivan Saez Zamora <ivsaez@gmail.com>
2022-03-09 08:10:11 +0100

    remove unneeded index

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
 export { IEmotional } from "./interfaces";
 export { EffectDirection, EffectKind, EffectStrength, EffectReaction } from "./enumerations";
 export { EffectComponent } from "./components";
-
-function hello(name: string = "Ivan"): string {
-  return `Hello, ${name}`;
-}
-
-export default hello;
+export { Effect } from "./effects";
